274e523c9bc6eb3f7d95acee2ff9be6d3d32c795
UIT19522006 <67238266+UIT19522006@users.noreply.github.com>
2020-12-08 00:21:28 +0700

    project image displayer

---
+++ ImageDisplayer/ImageDisplayer/ImageDisplayer.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ImageDisplayer
+{
+	partial class ImageDisplayer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.picMain = new System.Windows.Forms.PictureBox();
+			this.btPrev = new System.Windows.Forms.Button();
+			this.btNext = new System.Windows.Forms.Button();
+			this.btName = new System.Windows.Forms.Button();
+			this.lbNum = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// picMain
+			// 
+			this.picMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.picMain.Location = new System.Drawing.Point(70, 30);
+			this.picMain.Name = "picMain";
+			this.picMain.Size = new System.Drawing.Size(650, 340);
+			this.picMain.TabIndex = 0;
+			this.picMain.TabStop = false;
+			// 
+			// btPrev
+			// 
+			this.btPrev.Location = new System.Drawing.Point(5, 158);
+			this.btPrev.Name = "btPrev";
+			this.btPrev.Size = new System.Drawing.Size(59, 52);
+			this.btPrev.TabIndex = 1;
+			this.btPrev.Text = "Previous";
+			this.btPrev.UseVisualStyleBackColor = true;
+			this.btPrev.Click += new System.EventHandler(this.btPrev_Click);
+			this.btPrev.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ImageDisplayer_KeyDown);
+			// 
+			// btNext
+			// 
+			this.btNext.Location = new System.Drawing.Point(725, 158);
+			this.btNext.Name = "btNext";
+			this.btNext.Size = new System.Drawing.Size(59, 52);
+			this.btNext.TabIndex = 1;
+			this.btNext.Text = "Next";
+			this.btNext.UseVisualStyleBackColor = true;
+			this.btNext.Click += new System.EventHandler(this.btNext_Click);
+			this.btNext.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ImageDisplayer_KeyDown);
+			// 
+			// btName
+			// 
+			this.btName.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.btName.Location = new System.Drawing.Point(70, 2);
+			this.btName.Name = "btName";
+			this.btName.Size = new System.Drawing.Size(650, 23);
+			this.btName.TabIndex = 2;
+			this.btName.UseVisualStyleBackColor = true;
+			// 
+			// lbNum
+			// 
+			this.lbNum.AutoSize = true;
+			this.lbNum.Location = new System.Drawing.Point(2, 12);
+			this.lbNum.Name = "lbNum";
+			this.lbNum.Size = new System.Drawing.Size(29, 13);
+			this.lbNum.TabIndex = 3;
+			this.lbNum.Text = "Num";
+			// 
+			// ImageDisplayer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 371);
+			this.Controls.Add(this.lbNum);
+			this.Controls.Add(this.btName);
+			this.Controls.Add(this.btNext);
+			this.Controls.Add(this.btPrev);
+			this.Controls.Add(this.picMain);
+			this.Name = "ImageDisplayer";
+			this.Text = "Image Displayer";
+			this.SizeChanged += new System.EventHandler(this.ImageDisplayer_SizeChanged);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ImageDisplayer_KeyDown);
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ImageDisplayer_KeyPress);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ImageDisplayer_KeyUp);
+			((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox picMain;
+		private System.Windows.Forms.Button btPrev;
+		private System.Windows.Forms.Button btNext;
+		private System.Windows.Forms.Button btName;
+		private System.Windows.Forms.Label lbNum;
+	}
+}
+
---
+++ ImageDisplayer/ImageDisplayer/ImageDisplayer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageDisplayer
+{
+	public partial class ImageDisplayer : Form
+	{
+		List<string> allJpg = new List<string>();
+		int currentImage = 0;
+		public ImageDisplayer()
+		{
+			InitializeComponent();
+			getAllJpg();
+			Display();
+		}
+		public void getAllJpg()
+		{
+			DirectoryInfo d = new DirectoryInfo(Directory.GetCurrentDirectory());//Assuming Test is your Folder
+			FileInfo[] Files = d.GetFiles("*.jpg"); //Getting Text files
+			// Có thể sort nhiều kiểu ở đây
+			Array.Sort(Files, delegate (FileInfo f1, FileInfo f2)
+			{
+				return f2.LastWriteTime.CompareTo(f1.LastWriteTime);
+			});
+			foreach (FileInfo file in Files)
+			{
+				allJpg.Add(file.Name);
+			}
+		}
+		public void Display()
+		{
+			if (allJpg.Count == 0) return;
+			picMain.BackgroundImage = Bitmap.FromFile(allJpg[currentImage]);
+			this.btName.Text = allJpg[currentImage];
+			this.lbNum.Text = currentImage.ToString();
+		}
+
+		private void btNext_Click(object sender, EventArgs e)
+		{
+			currentImage++;
+			if(currentImage>=allJpg.Count)
+			{
+				currentImage = 0;
+			}
+			Display();
+		}
+		private void btPrev_Click(object sender, EventArgs e)
+		{
+			currentImage--;
+			if (currentImage <0)
+			{
+				currentImage = allJpg.Count-1;
+			}
+			
+			Display();
+		}
+
+		private void ImageDisplayer_SizeChanged(object sender, EventArgs e)
+		{
+			Control ct = (Control)sender;
+			this.btName.Size = new Size((ct.Size.Width - 140),23);
+			this.btNext.Location = new Point(ct.Size.Width - 90, ct.Size.Height / 2 - btNext.Size.Height);
+			this.btPrev.Location = new Point(5, ct.Size.Height / 2 - btPrev.Size.Height);
+			this.picMain.Size = new Size((ct.Size.Width - 140), ct.Size.Height - 70);
+		}
+
+		private void ImageDisplayer_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.D || e.KeyCode==Keys.Right)
+			{
+				currentImage++;
+				if (currentImage >= allJpg.Count)
+				{
+					currentImage = 0;
+				}
+			}
+			else if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+			{
+				currentImage--;
+				if (currentImage < 0)
+				{
+					currentImage = allJpg.Count - 1;
+				}
+			}
+			Display();
+		}
+
+		private void ImageDisplayer_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//if (e.KeyChar == Keys.Right)
+			//{
+			//	currentImage++;
+			//	if (currentImage >= allJpg.Count)
+			//	{
+			//		currentImage = 0;
+			//	}
+			//	Display();
+			//}
+			//else if (e.KeyChar == K)
+			//{
+			//	currentImage--;
+			//	if (currentImage <= 0)
+			//	{
+			//		currentImage = allJpg.Count - 1;
+			//	}
+			//	Display();
+			//}
+		}
+
+		private void ImageDisplayer_KeyUp(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.A)
+			{
+				currentImage++;
+				if (currentImage >= allJpg.Count)
+				{
+					currentImage = 0;
+				}
+			}
+			else if (e.KeyCode == Keys.D)
+			{
+				currentImage--;
+				if (currentImage <= 0)
+				{
+					currentImage = allJpg.Count - 1;
+				}
+			}
+			Display();
+		}
+	}
+}
